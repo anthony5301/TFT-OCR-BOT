0a5a8d2ead199d2c78961fcb1e01437503ba871f
Enterly <36875936+anthony5301@users.noreply.github.com>
2024-01-03 18:23:41 +0800

    Automatically elevate privilege to admin
    Now we can just double click main.py to run the bot and it will ask for admin privilege

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """
 Where the bot execution starts & contains the game loop that keeps the bot running indefinitely
 """
-
+import ctypes
+import sys
 import multiprocessing
 from ui import UI
 import auto_queue
@@ -15,15 +16,24 @@ def game_loop(ui_queue: multiprocessing.Queue) -> None:
         auto_queue.queue()
         Game(ui_queue)
 
+def is_admin():
+    """Check if bot is running as admin to prevent bot can't move in-game"""
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except: # pylint: disable=bare-except
+        return False
 
 if __name__ == "__main__":
     if settings.LEAGUE_CLIENT_PATH is None:
         raise ValueError("No league client path specified. Please set the path in settings.py")
-    message_queue = multiprocessing.Queue()
-    overlay: UI = UI(message_queue)
-    game_thread = multiprocessing.Process(target=game_loop, args=(message_queue,))
+    if is_admin():
+        message_queue = multiprocessing.Queue()
+        overlay: UI = UI(message_queue)
+        game_thread = multiprocessing.Process(target=game_loop, args=(message_queue,))
 
-    print("TFT OCR | https://github.com/jfd02/TFT-OCR-BOT")
-    print("Close this window to terminate the overlay window & program")
-    game_thread.start()
-    overlay.ui_loop()
+        print("TFT OCR | https://github.com/jfd02/TFT-OCR-BOT")
+        print("Close this window to terminate the overlay window & program")
+        game_thread.start()
+        overlay.ui_loop()
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
